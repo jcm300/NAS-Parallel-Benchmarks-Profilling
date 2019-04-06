328249f2e559a03f900732aaaf640aeef221f882
jcm300 <zeclmartins@gmail.com>
2019-04-06 20:15:25 +0100

    minor fixes

--- Scripts/IS/run.sh
+++ Scripts/IS/run.sh
@@ -6,50 +6,50 @@ function getB(){
 
     sar -r 1 -u > $folder/cpu_sar.txt &
     PID=$!
-    ../bin/is.A.x
+    ../bin/is.$4.x
     kill -9 $PID
 
     vmstat 1 > $folder/mem_vmstat.txt &
     PID=$!
-    ../bin/is.A.x
+    ../bin/is.$4.x
     kill -9 $PID
 
     pidstat -u 1 > $folder/cpu_pidstat.txt &
     PID=$!
-    ../bin/is.A.x
+    ../bin/is.$4.x
     kill -9 $PID
 
     pidstat -r 1 > $folder/mem_pidstat.txt &
     PID=$!
-    ../bin/is.A.x
+    ../bin/is.$4.x
     kill -9 $PID
 
     sar -r 1 > $folder/mem_sar.txt &
     PID=$!
-    ../bin/is.A.x
+    ../bin/is.$4.x
     kill -9 $PID
 
     iostat -d 1 > $folder/disk_iostat.txt &
     PID=$!
-    ../bin/is.A.x
+    ../bin/is.$4.x
     kill -9 $PID
 
     vmstat -d 1 > $folder/disk_vmstat.txt &
     PID=$!
-    ../bin/is.A.x
+    ../bin/is.$4.x
     kill -9 $PID
 
     sar -r 1 -n DEV > $folder/network_usage_sar.txt &
     PID=$!
-    ../bin/is.A.x
+    ../bin/is.$4.x
     kill -9 $PID
 
     sar -r 1 -n EDEV > $folder/network_saturation_sar.txt &
-    PID=($!)
-    ../bin/is.A.x
+    PID=$!
+    ../bin/is.$4.x
     kill -9 $PID
 
-    rm ../bin/is.A.x
+    rm ../bin/is.$4.x
 }
 
 function bench(){
@@ -62,9 +62,9 @@ function bench(){
     fi
 
     if [[ $vect -eq $3 ]]; then
-        make compiler=$1 opt=$2 vect=1 CLASS=$4 suite
+        make compiler=$1 opt=$2 vect=1 CLASS=$4
     else
-        make compiler=$1 opt=$2 CLASS=$4 suite
+        make compiler=$1 opt=$2 CLASS=$4
     fi
 
     getB $1 $2 $3 $4 $5 $6
@@ -78,32 +78,32 @@ function runBench(){
     #GNU compiler
 
     #-O1
-    bench GNU 1 0 A $1 $2
+    bench GNU 1 0 B $1 $2
     #-O2
-    bench GNU 2 0 A $1 $2
+    bench GNU 2 0 B $1 $2
     #-O3
-    bench GNU 3 0 A $1 $2
+    bench GNU 3 0 B $1 $2
     #-Os
-    bench GNU S 0 A $1 $2
+    bench GNU S 0 B $1 $2
     #-Ofast
-    bench GNU F 0 A $1 $2
+    bench GNU F 0 B $1 $2
     #-02 -ftree-vectorize
-    bench GNU 2 1 A $1 $2
+    bench GNU 2 1 B $1 $2
 
     #Intel compiler
 
     #-O1
-    bench INTEL 1 0 A $1 $2
+    bench INTEL 1 0 B $1 $2
     #-O2
-    bench INTEL 2 0 A $1 $2
+    bench INTEL 2 0 B $1 $2
     #-O3
-    bench INTEL 3 0 A $1 $2
+    bench INTEL 3 0 B $1 $2
     #-Os
-    bench INTEL S 0 A $1 $2
+    bench INTEL S 0 B $1 $2
     #-Ofast
-    bench INTEL F 0 A $1 $2
+    bench INTEL F 0 B $1 $2
     #-02 -ftree-vectorize
-    bench INTEL 2 1 A $1 $2
+    bench INTEL 2 1 B $1 $2
 }
 
 #SEQ
