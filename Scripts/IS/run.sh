ab8fd8d388aa3e833a553f4e7e201966cdf55cff
jcm300 <zeclmartins@gmail.com>
2019-04-06 13:58:16 +0100

    updated script, now done all necessary benchs for IS

--- Scripts/IS/run.sh
+++ Scripts/IS/run.sh
@@ -6,62 +6,113 @@
 #PBS -q mei
 
 function getB(){
-    sar -u 1 > sarU_$1_$2_$3_$4.txt &
+    folder=../../../../Scripts/IS/$5/$1_$2_$3_$4_results
+    mkdir -p $folder
+
+    sar -r 1 -u > $folder/cpu_sar.txt &
+    PID=$!
     ../bin/is.A.x
-    vmstat 1 > vmstat_$1_$2_$3_$4.txt &
+    kill -9 $PID
+
+    vmstat 1 > $folder/mem_vmstat.txt &
+    PID=$!
     ../bin/is.A.x
-    pidstat -u 1 > pidstatU_$1_$2_$3_$4.txt &
+    kill -9 $PID
+
+    pidstat -u 1 > $folder/cpu_pidstat.txt &
+    PID=$!
     ../bin/is.A.x
-    pidstat -r 1 > pidstatR_$1_$2_$3_$4.txt &
+    kill -9 $PID
+
+    pidstat -r 1 > $folder/mem_pidstat.txt &
+    PID=$!
     ../bin/is.A.x
-    vmstat -d 1 > vmstatD_$1_$2_$3_$4.txt &
+    kill -9 $PID
+
+    sar -r 1 > $folder/mem_sar.txt &
+    PID=$!
     ../bin/is.A.x
-    sar -n DEV > sarNDEV_$1_$2_$3_$4.txt &
+    kill -9 $PID
+
+    iostat -d 1 > $folder/disk_iostat.txt &
+    PID=$!
     ../bin/is.A.x
-    sar -n EDEV > sarNEDEV_$1_$2_$3_$4.txt &
+    kill -9 $PID
+
+    vmstat -d 1 > $folder/disk_vmstat.txt &
+    PID=$!
     ../bin/is.A.x
+    kill -9 $PID
+
+    sar -r 1 -n DEV > $folder/network_usage_sar.txt &
+    PID=$!
+    ../bin/is.A.x
+    kill -9 $PID
+
+    sar -r 1 -n EDEV > $folder/network_saturation_sar.txt &
+    PID=($!)
+    ../bin/is.A.x
+    kill -9 $PID
+
     rm ../bin/is.A.x
 }
 
 function bench(){
+    mkdir ../bin
+
+    if [[ $compiler -eq "GNU" ]]; then
+        module load gcc/7.2.0
+    else
+        source /share/apps/intel/parallel_studio_xe_2019/compilers_and_libraries_2019/linux/bin/compilervars.sh intel64
+    fi
+
     if [[ $vect -eq $3 ]]; then
         make compiler=$1 opt=$2 vect=1 CLASS=$4
     else
         make compiler=$1 opt=$2 CLASS=$4
     fi
 
-    getB $1 $2 $3 $4
+    getB $1 $2 $3 $4 $5
     make clean
 }
 
-cd ../../Kernels/SEQ/IS
+function runBench(){
+    cd ../../Benchmarks/IS_FT/$1/IS
 
-#GNU compiler
+    #GNU compiler
 
-#-O1
-bench GNU 1 0 A
-#-O2
-bench GNU 2 0 A
-#-O3
-bench GNU 3 0 A
-#-Os
-bench GNU S 0 A
-#-Ofast
-bench GNU F 0 A
-#-02 -ftree-vectorize
-bench GNU 2 1 A
+    #-O1
+    bench GNU 1 0 A $1
+    #-O2
+    bench GNU 2 0 A $1
+    #-O3
+    bench GNU 3 0 A $1
+    #-Os
+    bench GNU S 0 A $1
+    #-Ofast
+    bench GNU F 0 A $1
+    #-02 -ftree-vectorize
+    bench GNU 2 1 A $1
 
-#Intel compiler
+    #Intel compiler
 
-#-O1
-bench INTEL 1 0 A
-#-O2
-bench INTEL 2 0 A
-#-O3
-bench INTEL 3 0 A
-#-Os
-bench INTEL S 0 A
-#-Ofast
-bench INTEL F 0 A
-#-02 -ftree-vectorize
-bench INTEL 2 1 A
+    #-O1
+    bench INTEL 1 0 A $1
+    #-O2
+    bench INTEL 2 0 A $1
+    #-O3
+    bench INTEL 3 0 A $1
+    #-Os
+    bench INTEL S 0 A $1
+    #-Ofast
+    bench INTEL F 0 A $1
+    #-02 -ftree-vectorize
+    bench INTEL 2 1 A $1
+}
+
+#SEQ
+runBench NPB3.3-SER
+#OMP
+runBench NPB3.3-OMP
+#MPI
+runBench NPB3.3-MPI
