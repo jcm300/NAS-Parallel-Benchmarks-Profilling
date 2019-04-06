e292e82ffe964cc128d9aa2a5b1b64d4a2c5232c
jcm300 <zeclmartins@gmail.com>
2019-04-06 15:06:52 +0100

    added script to run on cluster

--- Scripts/IS/run.sh
+++ Scripts/IS/run.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
-#
-#PBS -N IS
-#PBS -l walltime=05:00
-#PBS -l nodes=1:ppn=1
-#PBS -q mei
 
 function getB(){
-    folder=../../../../Scripts/IS/$5/$1_$2_$3_$4_results
+    folder=../../../../Scripts/IS/$6/$5/$1_$2_$3_$4_results
     mkdir -p $folder
 
     sar -r 1 -u > $folder/cpu_sar.txt &
@@ -61,7 +56,8 @@ function bench(){
     mkdir ../bin
 
     if [[ $compiler -eq "GNU" ]]; then
-        module load gcc/7.2.0
+        #module load gcc/7.2.0
+        module load gcc/5.3.0 #o 7.2.0 estava a dar erro de load, usar este
     else
         source /share/apps/intel/parallel_studio_xe_2019/compilers_and_libraries_2019/linux/bin/compilervars.sh intel64
     fi
@@ -72,47 +68,48 @@ function bench(){
         make compiler=$1 opt=$2 CLASS=$4
     fi
 
-    getB $1 $2 $3 $4 $5
+    getB $1 $2 $3 $4 $5 $6
     make clean
 }
 
 function runBench(){
-    cd ../../Benchmarks/IS_FT/$1/IS
+    #cd ../../Benchmarks/IS_FT/$1/IS
+    cd Benchmarks/IS_FT/$1/IS #no Cluster ao fazer qsub tem de ser assim
 
     #GNU compiler
 
     #-O1
-    bench GNU 1 0 A $1
+    bench GNU 1 0 A $1 $2
     #-O2
-    bench GNU 2 0 A $1
+    bench GNU 2 0 A $1 $2
     #-O3
-    bench GNU 3 0 A $1
+    bench GNU 3 0 A $1 $2
     #-Os
-    bench GNU S 0 A $1
+    bench GNU S 0 A $1 $2
     #-Ofast
-    bench GNU F 0 A $1
+    bench GNU F 0 A $1 $2
     #-02 -ftree-vectorize
-    bench GNU 2 1 A $1
+    bench GNU 2 1 A $1 $2
 
     #Intel compiler
 
     #-O1
-    bench INTEL 1 0 A $1
+    bench INTEL 1 0 A $1 $2
     #-O2
-    bench INTEL 2 0 A $1
+    bench INTEL 2 0 A $1 $2
     #-O3
-    bench INTEL 3 0 A $1
+    bench INTEL 3 0 A $1 $2
     #-Os
-    bench INTEL S 0 A $1
+    bench INTEL S 0 A $1 $2
     #-Ofast
-    bench INTEL F 0 A $1
+    bench INTEL F 0 A $1 $2
     #-02 -ftree-vectorize
-    bench INTEL 2 1 A $1
+    bench INTEL 2 1 A $1 $2
 }
 
 #SEQ
-runBench NPB3.3-SER
+runBench NPB3.3-SER $1 
 #OMP
-runBench NPB3.3-OMP
+runBench NPB3.3-OMP $1
 #MPI
-runBench NPB3.3-MPI
+runBench NPB3.3-MPI $1
