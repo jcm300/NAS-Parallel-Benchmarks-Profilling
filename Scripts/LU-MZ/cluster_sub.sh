9c6fbaf7194a65cb66f86a43a23c04ea46fd1d03
MQuaresma <miguelquaresma97@gmail.com>
2019-04-15 22:17:19 +0100

    Submit script for LU-MZ benchmark

---
+++ Scripts/LU-MZ/cluster_sub.sh
@@ -0,0 +1,5 @@
+mkdir -p ~/LU-MZ_RESULTS
+
+qsub -N FTr641 -l nodes=1:r641:ppn=32 -l walltime=120:00 -q mei run.sh -F "FTr641"
+
+qsub -N FTr432 -l nodes=1:r432:ppn=24 -l walltime=120:00 -q mei run.sh -F "FTr432"
