21d4f6667895013083fd1d76cc79e9645169706b
jcm300 <zeclmartins@gmail.com>
2019-04-19 21:49:34 +0100

    more minor fixes

--- Scripts/getValues.sh
+++ Scripts/getValues.sh
@@ -3,7 +3,7 @@
 resFolder="res.txt"
 
 #cpu_pidstat %cpu
-echo "%cpu:" >> $resFolder
+echo "%CPU:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($8 ~ /(is|lu-mz)\.(A|B)\.(x|\d+)/){sub(/\./,",",$6); print $6}}' cpu_pidstat.txt >> $resFolder
 echo "" >> $resFolder
 
@@ -27,48 +27,48 @@ echo "%user:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($3 ~ /[0-9]+\.[0-9]+/){sub(/\./,",",$3); print $3}}' cpu_sar.txt >> $resFolder
 echo "" >> $resFolder
 
-#disk_iostat %Blk_read/s
-echo "%Blk_read/s:" >> $resFolder
+#disk_iostat Blk_read/s
+echo "Blk_read/s:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($3 ~ /[0-9]+\.[0-9]+/){sub(/\./,",",$3); print $3}}' disk_iostat.txt >> $resFolder
 echo "" >> $resFolder
 
-#disk_iostat %Blk_wrtn/s
-echo "%Blk_wrtn/s:" >> $resFolder
+#disk_iostat Blk_wrtn/s
+echo "Blk_wrtn/s:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($4 ~ /[0-9]+\.[0-9]+/){sub(/\./,",",$4); print $4}}' disk_iostat.txt >> $resFolder
 echo "" >> $resFolder
 
-#disk_vmstat %reads_merged
-echo "%reads_merged:" >> $resFolder
+#disk_vmstat reads_merged
+echo "reads_merged:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($1 ~ /sda/){sub(/\./,",",$3); print $3}}' disk_vmstat.txt >> $resFolder
 echo "" >> $resFolder
 
-#disk_vmstat %reads_ms
-echo "%reads_ms:" >> $resFolder
+#disk_vmstat reads_ms
+echo "reads_ms:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($1 ~ /sda/){sub(/\./,",",$5); print $5}}' disk_vmstat.txt >> $resFolder
 echo "" >> $resFolder
 
-#disk_vmstat %writes_merged
-echo "%writes_merged:" >> $resFolder
+#disk_vmstat writes_merged
+echo "writes_merged:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($1 ~ /sda/){sub(/\./,",",$7); print $7}}' disk_vmstat.txt >> $resFolder
 echo "" >> $resFolder
 
-#disk_vmstat %writes_ms
-echo "%writes_ms:" >> $resFolder
+#disk_vmstat writes_ms
+echo "writes_ms:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($1 ~ /sda/){sub(/\./,",",$9); print $9}}' disk_vmstat.txt >> $resFolder
 echo "" >> $resFolder
 
-#disk_vmstat %io_cur
-echo "%io_cur:" >> $resFolder
+#disk_vmstat io_cur
+echo "io_cur:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($1 ~ /sda/){sub(/\./,",",$10); print $10}}' disk_vmstat.txt >> $resFolder
 echo "" >> $resFolder
 
-#disk_vmstat %io_sec
-echo "%io_sec:" >> $resFolder
+#disk_vmstat io_sec
+echo "io_sec:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($1 ~ /sda/){sub(/\./,",",$11); print $11}}' disk_vmstat.txt >> $resFolder
 echo "" >> $resFolder
 
 #mem_pidstat %mem
-echo "%mem:" >> $resFolder
+echo "%MEM:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($8 ~ /(is|lu-mz)\.(A|B)\.(x|\d+)/){sub(/\./,",",$7); print $7}}' mem_pidstat.txt >> $resFolder
 echo "" >> $resFolder
 
@@ -78,76 +78,76 @@ awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($4 ~ /[0-9]+\.[0-9]+/){sub(/\./,","
 echo "" >> $resFolder
 
 #mem_vmstat swpd
-echo "%swpd:" >> $resFolder
+echo "swpd:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($4 ~ /[0-9]+/){sub(/\./,",",$4); print $4}}' mem_vmstat.txt >> $resFolder
 echo "" >> $resFolder
 
 #mem_vmstat free
-echo "%free:" >> $resFolder
+echo "free:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($5 ~ /[0-9]+/){sub(/\./,",",$5); print $5}}' mem_vmstat.txt >> $resFolder
 echo "" >> $resFolder
 
 #mem_vmstat buff
-echo "%buff:" >> $resFolder
+echo "buff:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($6 ~ /[0-9]+/){sub(/\./,",",$6); print $6}}' mem_vmstat.txt >> $resFolder
 echo "" >> $resFolder
 
 #mem_vmstat cache
-echo "%cache:" >> $resFolder
+echo "cache:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($7 ~ /[0-9]+/){sub(/\./,",",$7); print $7}}' mem_vmstat.txt >> $resFolder
 echo "" >> $resFolder
 
 #mem_vmstat si
-echo "%si:" >> $resFolder
+echo "si:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($8 ~ /[0-9]+/){sub(/\./,",",$8); print $8}}' mem_vmstat.txt >> $resFolder
 echo "" >> $resFolder
 
 #mem_vmstat so
-echo "%so:" >> $resFolder
+echo "so:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($9 ~ /[0-9]+/){sub(/\./,",",$9); print $9}}' mem_vmstat.txt >> $resFolder
 echo "" >> $resFolder
 
 #network_saturation rxdrop/s
-echo "%rxdrop/s:" >> $resFolder
+echo "rxdrop/s:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($2 ~ /eth1/){sub(/\./,",",$6); print $6}}' network_saturation_sar.txt >> $resFolder
 echo "" >> $resFolder
 
 #network_saturation txdrop/s
-echo "%txdrop/s:" >> $resFolder
+echo "txdrop/s:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($2 ~ /eth1/){sub(/\./,",",$7); print $7}}' network_saturation_sar.txt >> $resFolder
 echo "" >> $resFolder
 
 #network_saturation rxfifo/s
-echo "%rxfifo/s:" >> $resFolder
+echo "rxfifo/s:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($2 ~ /eth1/){sub(/\./,",",$10); print $10}}' network_saturation_sar.txt >> $resFolder
 echo "" >> $resFolder
 
 #network_saturation txfifo/s
-echo "%txfifo/s:" >> $resFolder
+echo "txfifo/s:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($2 ~ /eth1/){sub(/\./,",",$11); print $11}}' network_saturation_sar.txt >> $resFolder
 echo "" >> $resFolder
 
 #network_usage rxpck/s
-echo "%rxpck/s:" >> $resFolder
+echo "rxpck/s:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($2 ~ /eth1/){sub(/\./,",",$3); print $3}}' network_usage_sar.txt >> $resFolder
 echo "" >> $resFolder
 
 #network_usage txpck/s
-echo "%txpck/s:" >> $resFolder
+echo "txpck/s:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($2 ~ /eth1/){sub(/\./,",",$4); print $4}}' network_usage_sar.txt >> $resFolder
 echo "" >> $resFolder
 
 #network_usage rxkB/s
-echo "%rxkB/s:" >> $resFolder
+echo "rxkB/s:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($2 ~ /eth1/){sub(/\./,",",$5); print $5}}' network_usage_sar.txt >> $resFolder
 echo "" >> $resFolder
 
 #network_usage txkB/s
-echo "%txkB/s:" >> $resFolder
+echo "txkB/s:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($2 ~ /eth1/){sub(/\./,",",$6); print $6}}' network_usage_sar.txt >> $resFolder
 echo "" >> $resFolder
 
 #network_usage rxmcst/s
-echo "%rxmcst/s:" >> $resFolder
+echo "rxmcst/s:" >> $resFolder
 awk 'BEGIN {RS="\n"; FS="\\s+"} $0 != "" {if($2 ~ /eth1/){sub(/\./,",",$9); print $9}}' network_usage_sar.txt >> $resFolder
 echo "" >> $resFolder
